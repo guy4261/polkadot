73d4ec4f250cbbef08d39fa51f2120e9e13ad1c9
Guy Rapaport <guyr@ziprecruiter.com>
2022-11-30 04:33:19 +0200

    Good bad ugly example in txt

--- examples/good_bad_ugly/good_bad_ugly.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-lines = open(__file__).readlines()
-open(__file__, "w").writelines(lines[:4] + [lines[5], lines[4], lines[6]])
-print('Good')
-print()
-print('Ugly')
-print("Goodbye now!")
